d73825c476fab017eab5a9ca896bae167f357bad
Samuka lindão <moreiranascimentosamuel4@gmail.com>
2025-10-08 11:31:44 -0300

    exercicio 004

--- exercicio2.py
+++
@@ -1,2 +0,0 @@
-nome= input ('Seja Bem vindo,Qual o seu nome?')
-print ( 'É um prazer te conhecer!!, {} !' .format (nome))
---
+++ exercicios.py
@@ -0,0 +1,48 @@
+"Exercicio-1"
+#print ('Olá mundo')
+
+"Exercio-2"
+# nome= input ('Seja Bem vindo,Qual o seu nome?')
+# print ( 'É um prazer te conhecer!!, {} !' .format (nome))
+
+"Exercicio-3"
+#print ('--__desaio= soma__--_')
+#ni1=  int (input ('Digite um numero'))
+#ni2= int (input('Digite um numero'))
+
+#soma = ni1 + ni2
+#print ( 'a soma dos numeros é',soma )
+"ou"
+#print("a soma dos numeros {} e {} vale = {}".format(ni1,ni2,soma))
+ 
+#Números#
+num=(int) = -7,7,-87
+num=(float) = 6.4,6.34,7.0
+num=(bool) = True,False
+num=(str) = ('Texto, ex:Olá')
+
+'Exemplos'
+(1)
+#n= input ('Digite algo :') 
+#print(n.isnumeric())
+#confere se é numero ou não
+(2)
+#oi= input ('Digite algo :')
+#print(oi.isalpha())
+#confere se é letra
+(3)
+#opa3= input ('Digite algo:')
+#print(opa3.isalnum())
+#confere se é letra e numero junto
+(4)
+#opa= input ('Digite algo:')
+#print(type(opa.isupper()))
+#confere se esta maiusculo
+Atenção=('Colocar depois todos os is')
+
+
+'Exercicio-4'
+print('_--_Desafio 04_--_')
+opa= input ('Digite algo:')
+print(type(opa.isupper()))
+
