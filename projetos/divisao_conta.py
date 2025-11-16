6b59923bf2b331782380db41ac352ce4d207d307
Samuka lindão <moreiranascimentosamuel4@gmail.com>
2025-11-16 20:31:21 -0300

    divisor de conta

---
+++ projetos/divisao_conta.py
@@ -0,0 +1,16 @@
+#print('Divisão De Conta')
+#pergunta=int(input('Quantas pessoas irão dividir está conta ? '))
+#total=float(input('Qual o valor total da conta ? R$ '))
+#calcula= total/pergunta
+#print(f'O Valor a ser dividido é R${calcula :.2f}')
+
+'Versão que só aceita numeros possiveis'
+#while True:
+ #pergunta=int(input('Quantas pessoas irão dividir está conta ? '))
+ #if pergunta <= 0:
+  #print('Este Valor não se pode ser divido ')
+  #continue
+ #total=float(input('Qual o valor total da conta ? R$ '))
+ #calcula= total/pergunta
+ #print(f'O Valor a ser dividido é R${calcula :.2f}')
+ #break
